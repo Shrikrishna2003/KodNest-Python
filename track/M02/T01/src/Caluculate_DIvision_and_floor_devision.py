9cb8eb859d47fcef37a909e31f7fbaa10e56d639
Shrikrishna2003 <shrikrishnamokhashi2003@gmail.com>
2026-08-04 22:46:39 +0530

    Calculate Division and floor Division

---
+++ track/M02/T01/src/Caluculate_DIvision_and_floor_devision.py
@@ -0,0 +1,7 @@
+# Calculate Division and floor Division
+first_number = int(input())
+second_number = int(input())
+result1 = first_number / second_number
+result2 = first_number // second_number
+print(result1)
+print(result2)
