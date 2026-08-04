884d498c994c3d06b0ee8af8dd68b6f1bfed96c9
Shrikrishna2003 <shrikrishnamokhashi2003@gmail.com>
2026-08-04 22:47:49 +0530

    Read and convert the student details

---
+++ track/M02/T01/src/Read_and_convert_student_details.py
@@ -0,0 +1,14 @@
+# Read and convert the student details
+Student_name = input()
+Student_age = int(input())
+course_rating = float(input())
+
+# Display the values
+print(f"Student: {Student_name}")
+print( f"Age: {Student_age}")
+print(f"Rating: {course_rating}")
+
+# Display the data types
+print(type(Student_name))
+print(type(Student_age))
+print(type(course_rating))
