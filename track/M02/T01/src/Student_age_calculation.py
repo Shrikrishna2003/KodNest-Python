8f0d7c4ee652854b53ea9afdc5a1f26d08cb078d
Shrikrishna2003 <shrikrishnamokhashi2003@gmail.com>
2026-08-04 22:32:19 +0530

    Read and convert the student details

---
+++ track/M02/T01/src/Student_age_calculation.py
@@ -0,0 +1,6 @@
+# Calculate student age next year by converting string to int .
+current_age = "21"
+current_age = int(current_age)
+next_age = current_age + 1
+
+print(f"Age next year: {next_age}")
